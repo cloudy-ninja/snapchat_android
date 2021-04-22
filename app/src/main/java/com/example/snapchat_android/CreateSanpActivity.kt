091cc4272a439c513b09b51ec50c845b0dea684d
cloudy-ninja <msr0327@outlook.com>
2021-04-22 05:17:11 -0500

    request photo permission

--- app/src/main/java/com/example/snapchat_android/CreateSanpActivity.kt
+++ app/src/main/java/com/example/snapchat_android/CreateSanpActivity.kt
@@ -1,11 +1,41 @@
 package com.example.snapchat_android
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class CreateSanpActivity : AppCompatActivity() {
+    var imageView: ImageView? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_sanp)
+
+        imageView = findViewById(R.id.imageView);
     }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun onChooseMedia(view: View) {
+        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                1
+            );
+        } else {
+            getPhoto()
+        }
+    }
+
+    private fun getPhoto() {
+        var intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+    }
+
+
 }
